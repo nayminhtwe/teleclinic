d4760e863fca0c550910eaf252cbbb8033519a9d
naymin <konayminhtwe@gmail.com>
2021-02-20 17:40:09 +0630

    patient profile

--- src/layouts/partials/Header/DetailHeader.vue
+++ src/layouts/partials/Header/DetailHeader.vue
@@ -37,14 +37,14 @@
           <!-- <img src="https://cdn.quasar.dev/logo/svg/quasar-logo.svg" /> -->
           <img src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50.jpg" />
         </q-avatar>
-        <div class="text-h6 q-ml-sm">0000</div>
+        <div class="text-h6 q-ml-sm">EZ {{ patient.id }}</div>
       </div>
       <div class="col-8 col-lg-4">
-        <div class="text-h6">Aye Aye Myint</div>
-        <div class="text-subtitle2">32 years</div>
-        <div class="text-subtitle2">Female</div>
-        <div class="text-weight-regular">12 street,34 ward,56 City,78 Ts</div>
-        <div class="text-weight-regular">09-123456789</div>
+        <div class="text-h6">{{ patient.Name }}</div>
+        <div class="text-subtitle2">{{ patient.Age}} years</div>
+        <div class="text-subtitle2">{{ patient.Gender }}</div>
+        <div class="text-weight-regular">{{ patient.Address }}</div>
+        <div class="text-weight-regular">{{ patient.Contact_Number}}</div>
       </div>
       <!-- </div> -->
     </q-toolbar>
@@ -52,9 +52,26 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
   data () {
-    return {}
+    return {
+      patient: {}
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getDoctorToken: 'doctor/getDoctorToken'
+    })
+  },
+  created () {
+    this.$api.defaults.headers.Authorization = `Bearer ${this.getDoctorToken}`
+    this.$api.get(
+      `patient/${this.$route.params.patientId}`
+    ).then((response) => {
+      this.patient = response.data.data
+    })
   }
 }
 </script>

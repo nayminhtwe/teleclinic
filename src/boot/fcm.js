a0b8f86c8b038c53da499c248e36345c32a48dd6
naymin <konayminhtwe@gmail.com>
2021-07-05 23:44:29 +0630

    fcm package adjust

--- src/boot/fcm.js
+++ src/boot/fcm.js
@@ -3,11 +3,13 @@
 import { Platform } from 'quasar'
 
 if (Platform.is.cordova) {
-    cordova.plugins.firebase.messaging.onMessage(function (payload) {
-        console.log('New foreground FCM message: ', payload)
-    })
+    document.addEventListener('deviceready', () => {
+        cordova.plugins.firebase.messaging.onMessage(function (payload) {
+            console.log('New foreground FCM message: ', payload)
+        })
 
-    cordova.plugins.firebase.messaging.onBackgroundMessage(function (payload) {
-        console.log('New background FCM message: ', payload)
-    })
+        cordova.plugins.firebase.messaging.onBackgroundMessage(function (payload) {
+            console.log('New background FCM message: ', payload)
+        })
+    }, false)
 }

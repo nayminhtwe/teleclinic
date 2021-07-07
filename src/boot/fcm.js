ef5baccf63c49ef9bd7b2e9d16dde6c081320eb1
naymin <konayminhtwe@gmail.com>
2021-07-07 14:23:00 +0630

    clear noti & patient create in home fix

--- src/boot/fcm.js
+++ src/boot/fcm.js
@@ -9,7 +9,9 @@ if (Platform.is.cordova) {
         })
 
         cordova.plugins.firebase.messaging.onBackgroundMessage(function (payload) {
-            console.log('New background FCM message: ', payload)
+            cordova.plugins.firebase.messaging.clearNotifications(function () {
+                console.log('Notification messages cleared successfully')
+            })
         })
     }, false)
 }

b78506d9f4fc8e48a8cbf7860d166fb0826a50bb
naymin <konayminhtwe@gmail.com>
2021-09-28 16:00:37 +0630

    profile edit

--- src/pages/Profile.vue
+++ src/pages/Profile.vue
@@ -450,6 +450,152 @@
       class="q-pa-md"
       v-if="getDoctorProfile.status == 1 || getDoctorProfile.status == 3"
     >
+      <q-dialog v-model="doctor_edit">
+        <q-card>
+          <q-card-section>
+            <div class="text-h6">Doctor Profile Edit</div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <div class="row q-gutter-md">
+              <q-banner
+                inline-actions
+                class="text-white bg-red"
+                v-if="banner"
+              >
+                {{ message }}
+                <template v-slot:action>
+                  <q-btn
+                    flat
+                    color="white"
+                    icon="error"
+                    @click="banner = false"
+                  />
+                </template>
+              </q-banner>
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="name"
+                  label="Name"
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="sama_number"
+                  label="SAMA No."
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="qualifications"
+                  label="Qualifications"
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-select
+                  v-model="specialization"
+                  label="Specializations"
+                  :options="specializations"
+                  :rules="[val => !!val || 'Field is required']"
+                  :option-label="(item) => item === null ? null : item.Name"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-select
+                  multiple
+                  v-model="prfered_languages"
+                  use-chips
+                  stack-label
+                  label="Language"
+                  :options="languages"
+                  :option-label="(item) => item === null ? null : item.language"
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="contact_number"
+                  label="Phone No."
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="email"
+                  label="Email"
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="time"
+                  label="Available Time"
+                  :rules="[val => !!val || 'Field is required']"
+                />
+              </div>
+
+              <div class="col-lg-4 col-12">
+                <q-input
+                  v-model="other"
+                  label="Comment"
+                />
+              </div>
+              <div class="col-12 offset-6 q-my-md">
+                <q-file
+                  borderless
+                  v-model="profile_image"
+                >
+                  <template v-slot:prepend>
+                    <img
+                      src="~assets/profile_upload.jpg"
+                      style="width: 72px"
+                      @click.stop
+                    />
+                  </template>
+                </q-file>
+              </div>
+              <div class="col-12 offset-6 q-my-md">
+                <q-file
+                  borderless
+                  v-model="sama_or_nrc"
+                >
+                  <template v-slot:prepend>
+                    <img
+                      src="~assets/certificate.png"
+                      style="width: 72px"
+                      @click.stop
+                    />
+                  </template>
+                </q-file>
+              </div>
+            </div>
+          </q-card-section>
+          <q-card-actions align="right">
+            <q-btn
+              flat
+              label="Cancel"
+              color="primary"
+              v-close-popup
+            />
+            <q-btn
+              flat
+              label="Edit"
+              color="primary"
+              @click="update"
+              v-close-popup
+            />
+          </q-card-actions>
+        </q-card>
+      </q-dialog>
       <div class="row q-gutter-md">
         <q-banner
           inline-actions
@@ -574,6 +720,18 @@
           >
         </div>
       </div>
+      <div class="q-py-md q-gutter-sm">
+        <q-btn
+          color="red"
+          class="text-white full-width"
+          rounded
+          @click="doctor_profile"
+        >
+          <div class="ellipsis">
+            Edit
+          </div>
+        </q-btn>
+      </div>
     </div>
 
     <div class="q-py-md q-gutter-sm">
@@ -626,7 +784,8 @@ export default {
       patient_gender: '',
       patient_address: '',
       patient_phone: '',
-      patient_profile_image: ''
+      patient_profile_image: '',
+      doctor_edit: false
     }
   },
   components: {
@@ -692,7 +851,7 @@ export default {
             // this.$q.notify('Your registration is successful. The EZ Care admin team will review your registration. Thank you.')
             this.$q.notify({
               type: 'positive',
-              message: 'Your registration is successful. <br />The EZ Care admin team will review your registration. <br />Thank you.',
+              message: 'Your registration is successful. <br />You can now search and talk with EZ care doctors.',
               html: true
             })
             this.$router.push('/')
@@ -702,6 +861,52 @@ export default {
         })
     },
     async submit () {
+      const formData = this.getFormdata()
+      this.$api.defaults.headers.Authorization = `Bearer ${this.getDoctorToken}`
+      this.$api.post('doctor_register', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+        .then((response) => {
+          if (response.data.error_code === '0') {
+            // this.$q.notify('Your registration is successful. You can now search and talk with EZ care doctors.')
+            this.$q.notify({
+              type: 'positive',
+              message: 'Your registration is successful. <br />The EZ Care admin team will review your registration. <br />Thank you.',
+              html: true
+            })
+            this.$router.push('/')
+          }
+        }).catch(err => {
+          console.log(err.response.data)
+          this.$q.notify({
+            type: 'negative',
+            message: err.response.data.message
+          })
+        })
+    },
+    async checkTOR () {
+      await this.$api.get('terms_of_reference')
+        .then((response) => {
+          this.tor = response.data.data
+        }).catch(err => {
+          console.log(err.response.data)
+        })
+      this.tor_popup = true
+    },
+    async doctor_profile () {
+      await this.fetch_doctor()
+      this.doctor_edit = true
+    },
+    async fetch_doctor () {
+      this.name = this.getDoctorProfile.name
+      this.sama_number = this.getDoctorProfile.sama_number
+      this.qualifications = this.getDoctorProfile.qualifications
+      this.specialization = this.specializations.filter(s => s.Name === this.getDoctorProfile.specialization)[0]
+      this.prfered_languages = this.getDoctorProfile.available_language
+      this.contact_number = this.getDoctorProfile.contact_number
+      this.email = this.getDoctorProfile.email
+      this.time = this.getDoctorProfile.available_time
+      this.other = this.getDoctorProfile.other_option
+    },
+    getFormdata () {
       const formData = new FormData()
       formData.append('Name', this.name)
       formData.append('sama_number', this.sama_number)
@@ -726,14 +931,17 @@ export default {
       formData.append('file_name', 'certificate')
       formData.append('hide_my_info', this.hide)
 
+      return formData
+    },
+    async update () {
+      const formData = this.getFormdata()
       this.$api.defaults.headers.Authorization = `Bearer ${this.getDoctorToken}`
-      this.$api.post('doctor_register', formData, { headers: { 'Content-Type': 'multipart/form-data' } })
+      this.$api.post(`doctor_edit/${this.getDoctorProfile.id}`, formData, { headers: { 'Content-Type': 'multipart/form-data' } })
         .then((response) => {
           if (response.data.error_code === '0') {
-            // this.$q.notify('Your registration is successful. You can now search and talk with EZ care doctors.')
             this.$q.notify({
               type: 'positive',
-              message: 'Your registration is successful. <br />You can now search and talk with EZ care doctors.',
+              message: 'Your Profile is successful updated',
               html: true
             })
             this.$router.push('/')
@@ -745,15 +953,6 @@ export default {
             message: err.response.data.message
           })
         })
-    },
-    async checkTOR () {
-      await this.$api.get('terms_of_reference')
-        .then((response) => {
-          this.tor = response.data.data
-        }).catch(err => {
-          console.log(err.response.data)
-        })
-      this.tor_popup = true
     }
   }
 }
